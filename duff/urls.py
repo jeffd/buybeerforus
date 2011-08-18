bd4a70ec90927398937952e4803a8d160c1159a8
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2011-08-18 19:11:51 -0400

    Update URLs to point to template

--- duff/urls.py
+++ duff/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # url(r'^myapp/', include('myapp.urls')),
-    
+    url(r'^$', direct_to_template, {"template": "home.html"}, name="home"),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
