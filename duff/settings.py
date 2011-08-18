243b8262cc4578cddf85fc370c4e900c3ff39427
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2011-08-18 19:44:41 -0400

    Added STATIC_URL

--- duff/settings.py
+++ duff/settings.py
@@ -28,6 +28,8 @@ MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media/')
 
 STATIC_ROOT = os.path.join(PROJECT_PATH, 'static/')
 
+STATIC_URL = '/assets/'
+
 STATICFILES_DIRS = (
     os.path.join(PROJECT_PATH, 'assets/'),
 )
